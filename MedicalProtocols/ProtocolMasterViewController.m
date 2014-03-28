89d6558fec8e54dbdfe537f656ec72b1a5d38f55
Lowell D. Olson <lowell@wrls-104-150.sonoma.edu>
2014-03-28 14:41:09 -0700

    Added a couple of test objects to View did load function to test parse database.

--- MedicalProtocols/ProtocolMasterViewController.m
+++ MedicalProtocols/ProtocolMasterViewController.m
@@ -33,9 +33,14 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];
+    
+    //This object is specifically for testing connectivity to the parse database  LDO
+    PFObject *patientObject = [PFObject objectWithClassName:@"patientObject"];
+    patientObject[@"LName"] = @"Jones";
+    patientObject[@"FName"] = @"Davie";
+    patientObject[@"AddressLine1"] = @"2112 Boulder Creek Lane";
+    patientObject[@"AddressLine2"] = @"Rohnert Park CA, 99922";
+    [patientObject saveInBackground];
     
     //Navigation Button Items removed
     //self.navigationItem.leftBarButtonItem = self.editButtonItem;
