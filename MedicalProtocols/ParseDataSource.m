bae3b44524c81a4a1281b8c854ce169c1ae62f9e
Lowell D. Olson <lowell@wrls-105-156.sonoma.edu>
2014-04-05 15:34:13 -0700

    Committing problems to the parse database

--- MedicalProtocols/ParseDataSource.m
+++ MedicalProtocols/ParseDataSource.m
@@ -9,13 +9,13 @@
 #import "ParseDataSource.h"
 #import <Parse/Parse.h>
 #import "MedProtocol.h"
-//#import "ProtocolStep.h"
-//#import "TextBlock.h"
-//#import "Link.h"
-//#import "Calculator.h"
-//#import "Form.h"
-//#import "FormNumber.h"
-//#import "FormSelection.h"
+#import "ProtocolStep.h"
+#import "TextBlock.h"
+#import "Link.h"
+#import "Calculator.h"
+#import "Form.h"
+#import "FormNumber.h"
+#import "FormSelection.h"
 
 @interface ParseDataSource()
 -(NSMutableArray*)getAllFromParseClassNamed:(NSString*)className;
@@ -23,13 +23,7 @@
 @end
 
 @implementation ParseDataSource
-+(ParseDataSource *)sharedInstance
-{
-    static ParseDataSource* sharedObject = nil;
-    if(sharedObject == nil)
-        sharedObject = [[ParseDataSource alloc] init];
-    return sharedObject;
-}
+
 -(NSArray*)getAll:(DataType)dataType{
     
 //    DataTypeProtocol,
@@ -43,23 +37,30 @@
             dataTypes = [self getAllFromParseClassNamed:@"Protocol"];
             break;
             
+        case DataTypeStep:
+            dataTypes = [self getAllFromParseClassNamed:@"Step"];
+            break;
+            
+        case DataTypeComponent:
+            dataTypes = [self getAllFromParseClassNamed:@"Component"];
+            break;
+            
+        case DataTypeFormComponent:
+            dataTypes = [self getFormComponents];
+            break;
+            
         default:
             break;
     }
-    
-//    switch (dataType) {
-//        case DataTypeProtocol:
-//            NSMutableArray* protocols = [[NSMutableArray alloc] init];
-//            dataTypes
-//            
-//            
-//            break;
-//            
-//        default:
-//            break;
-//    }
-    
     return dataTypes;
+}
+-(NSMutableArray*)getFormComponents{
+    NSArray* parseClassNames = @[@"FromNumber",@"FormSelection"];
+    NSMutableArray* formComponents = [[NSMutableArray alloc]  init];
+    for(NSString* className in parseClassNames){
+        [formComponents addObjectsFromArray:[self getAllFromParseClassNamed:className]];
+    }
+    return formComponents;
 }
 -(NSArray*)getAll:(DataType)dataType withParentId:(NSString*)parentId{
     return NULL;
@@ -76,34 +77,44 @@
 -(bool)getObjectDataType:(DataType)dataType withId:(NSString*)idString{
     return NULL;
 }
+
 -(NSMutableArray*)getAllFromParseClassNamed:(NSString*)className{
-//    NSMutableArray* protocols = [[NSMutableArray alloc] init];
-//    PFQuery *query = [PFQuery queryWithClassName:@"Protocol"];
-//    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-//        if (!error) {
-//            // The find succeeded.
-//            NSLog(@"Successfully retrieved %d sprotocols.", objects.count);
-//            // Do something with the found objects
-//            for (PFObject *object in objects) {
-//                //[_protocols addObject:[[MedProtocol alloc] initWithParseObject:object]];
-//            }
-//        }
-//    }
-    return NULL;
+    NSMutableArray* protocols = [[NSMutableArray alloc] init];
+    PFQuery *query = [PFQuery queryWithClassName:className];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            NSLog(@"Successfully retrieved %d sprotocols.", objects.count);
+            // Do something with the found objects
+            for (PFObject *object in objects) {
+                [protocols addObject:[[MedProtocol alloc] initWithParseObject:object]];
+            }
+        }
+    }];
+    return protocols;
 }
 
-//                MedProtocol *mp = [[MedProtocol alloc]init];
-//                mp.name =object[@"name"];
-//                mp.idStr = object.objectId;
-//                mp.createdAt = object.createdAt;
-//                mp.updatedAt = object.updatedAt;
-//                [protocols addObject:mp];
-//                //[self insertProtocol:mp];
-//                
-//            }
-//        } else {
-//            //Log details of the failure
-//            NSLog(@"Error: %@ %@", error, [error userInfo]);
-//        }
-//    }];
+-(NSMutableArray*)getAllStepsFromParseClassNamed:(NSString*)className{
+    NSMutableArray* steps = [[NSMutableArray alloc] init];
+    PFQuery *query = [PFQuery queryWithClassName:className];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            NSLog(@"Successfully retrieved %d sprotocols.", objects.count);
+            // Do something with the found objects
+            for (PFObject *object in objects) {
+                [steps addObject:[[ProtocolStep alloc] initWithParseObject:object]];
+            }
+        }
+    }];
+    return steps;
+}
+
++(ParseDataSource *)sharedInstance
+{
+    static ParseDataSource* sharedObject = nil;
+    if(sharedObject == nil)
+        sharedObject = [[ParseDataSource alloc] init];
+    return sharedObject;
+}
 @end
