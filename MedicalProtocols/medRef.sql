75d8c8288e211240810af284c8b82a2882fa9b9d
Lowell D. Olson <lowell@wrls-104-124.sonoma.edu>
2014-04-09 15:29:09 -0700

    New Database Schema with new id’s for objects

--- MedicalProtocols/medRef.sql
+++ MedicalProtocols/medRef.sql
@@ -5,86 +5,86 @@ BEGIN;
 DROP TABLE IF EXISTS protocol;
 
 CREATE TABLE protocol (
-    objectID	      varchar(20)	NOT NULL PRIMARY KEY,
-    updatedAt      date		NOT NULL,
-    pName	      varchar(50)	NOT NULL
+    id	   integer      NOT NULL PRIMARY KEY,
+    updatedAt      date         NOT NULL,
+    pName          varchar(50)	NOT NULL
 );
 
 DROP TABLE IF EXISTS step;
 
 CREATE TABLE step (
-    objectID		varchar(20)	NOT NULL PRIMARY KEY,
+    id          integer     NOT NULL PRIMARY KEY,
     stepNumber 	integer 	NOT NULL,
     updatedAt  	date		NOT NULL,
-    protocolID	varchar(20)	NOT NULL,
+    protocolId	integer     NOT NULL,
     description	varchar(200),
-    FOREIGN KEY (protocolID) REFERENCES protocol(objectID)
+    FOREIGN KEY (protocolId) REFERENCES protocol(id)
 );
    
 DROP TABLE IF EXISTS textBlock;
 
 CREATE TABLE textBlock (
-    objectID        varchar(20)	NOT NULL PRIMARY KEY,
+    id        integer     NOT NULL PRIMARY KEY,
     updatedAt       date		NOT NULL,
     printable       BIT 		NOT NULL,
     title	       varchar(100)	NOT NULL,
-    stepID          varchar(20) NOT NULL,
-    FOREIGN KEY (stepID) REFERENCES step(objectID)
+    stepId         integer      NOT NULL,
+    FOREIGN KEY (stepId) REFERENCES step(id)
 );
 
 DROP TABLE IF EXISTS calculator;
 
 CREATE TABLE calculator(
-    objectID		varchar(20)	NOT NULL PRIMARY KEY,
+    id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
     updatedAt       date		NOT NULL,
-    stepID          varchar(20) NOT NULL,
-    FOREIGN KEY (stepID) REFERENCES step(objectID)
+    stepId          integer     NOT NULL,
+    FOREIGN KEY (stepId) REFERENCES step(id)
 );
 
 DROP TABLE IF EXISTS link;
 
 CREATE TABLE link (
-    objectID        varchar(20)	NOT NULL PRIMARY KEY,
+    id              integer	NOT NULL PRIMARY KEY AUTOINCREMENT,
     url             varchar(100)      NOT NULL,
     updatedAt       date		NOT NULL,
     printable       BIT 		NOT NULL,
     label	       varchar(100)	NOT NULL,
-    stepID          varchar(20) NOT NULL,
-    FOREIGN KEY (stepID) REFERENCES step(objectID)
+    stepId         integer      NOT NULL,
+    FOREIGN KEY (stepId) REFERENCES step(id)
 );
 
 DROP TABLE IF EXISTS form;
 
 CREATE TABLE form(
-    objectID		varchar(20)	NOT NULL PRIMARY KEY,
+    id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
     updatedAt       date		NOT NULL,
-    stepID          varchar(20) NOT NULL,
-    FOREIGN KEY (stepID) REFERENCES step(objectID)
+    stepID          integer     NOT NULL,
+    FOREIGN KEY (stepId) REFERENCES step(id)
 );
 
 DROP TABLE IF EXISTS formSelection;
 
 CREATE TABLE formSelection(
-    objectID		varchar(20)	NOT NULL PRIMARY KEY,
+    id          integer         NOT NULL PRIMARY KEY AUTOINCREMENT,
     choiceA		varchar(1)      NOT NULL,
     choiceB		varchar(1)      NOT NULL,
     label		varchar(20)     NOT NULL,
     updatedAt       date		NOT NULL,
-    formID          varchar(20) NOT NULL,
-    FOREIGN KEY (formID) REFERENCES form(formID)
+    formId      integer         NOT NULL,
+    FOREIGN KEY (formId) REFERENCES form(id)
 );
 
 DROP TABLE IF EXISTS formNumber;
 
 CREATE TABLE formNumber(
-  objectID		varchar(20)     NOT NULL PRIMARY KEY,
+    id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
     defaultValue	integer		NOT NULL,
     minValue		integer		NOT NULL,
     maxValue		integer		NOT NULL,
     label 		varchar(50)     NOT NULL,
     updatedAt       date		NOT NULL,
-    formID          varchar(20) NOT NULL,
-    FOREIGN KEY (formID) REFERENCES form(formID)
+    formId          integer     NOT NULL,
+    FOREIGN KEY (formId) REFERENCES form(id)
 );
 
 COMMIT;
