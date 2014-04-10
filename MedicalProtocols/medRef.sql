0eb32b16bcab5b5e90325d5dcefda8399945539b
Lowell D. Olson <lowell@wrls-104-124.sonoma.edu>
2014-04-09 18:10:54 -0700

    Committing yet another id…

--- MedicalProtocols/medRef.sql
+++ MedicalProtocols/medRef.sql
@@ -5,14 +5,16 @@ BEGIN;
 DROP TABLE IF EXISTS protocol;
 
 CREATE TABLE protocol (
-    id	   integer      NOT NULL PRIMARY KEY,
-    pName          varchar(50)	NOT NULL
+    id                  integer      NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId            integer      NOT NULL,
+    pName               varchar(50)	NOT NULL
 );
 
 DROP TABLE IF EXISTS step;
 
 CREATE TABLE step (
-    id          integer     NOT NULL PRIMARY KEY,
+    id          integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId    integer      NOT NULL,
     stepNumber 	integer 	NOT NULL,
     protocolId	integer     NOT NULL,
     description	varchar(200)
@@ -21,7 +23,8 @@ CREATE TABLE step (
 DROP TABLE IF EXISTS textBlock;
 
 CREATE TABLE textBlock (
-    id        integer     NOT NULL PRIMARY KEY,
+    id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId        integer     NOT NULL,
     printable       BIT 		NOT NULL,
     title	       varchar(100)	NOT NULL,
     stepId         integer      NOT NULL
@@ -31,6 +34,7 @@ DROP TABLE IF EXISTS calculator;
 
 CREATE TABLE calculator(
     id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId        integer      NOT NULL,
     stepId          integer     NOT NULL
 );
 
@@ -38,6 +42,7 @@ DROP TABLE IF EXISTS link;
 
 CREATE TABLE link (
     id              integer	NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId            integer      NOT NULL,
     url             varchar(100)      NOT NULL,
     printable       BIT 		NOT NULL,
     label	       varchar(100)	NOT NULL,
@@ -48,6 +53,7 @@ DROP TABLE IF EXISTS form;
 
 CREATE TABLE form(
     id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId        integer      NOT NULL,
     stepID          integer     NOT NULL
 );
 
@@ -55,6 +61,7 @@ DROP TABLE IF EXISTS formSelection;
 
 CREATE TABLE formSelection(
     id          integer         NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId    integer      NOT NULL,
     choiceA		varchar(1)      NOT NULL,
     choiceB		varchar(1)      NOT NULL,
     label		varchar(20)     NOT NULL,
@@ -65,6 +72,7 @@ DROP TABLE IF EXISTS formNumber;
 
 CREATE TABLE formNumber(
     id              integer     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    objectId        integer      NOT NULL,
     defaultValue	integer		NOT NULL,
     minValue		integer		NOT NULL,
     maxValue		integer		NOT NULL,
