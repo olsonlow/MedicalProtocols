a8961653c8be0a5dade6293c2d29a4b6f8985a78
Lowell D. Olson <lowell@wrls-106-60.sonoma.edu>
2014-04-14 16:45:28 -0700

    Detail View repaired

--- MedicalProtocols/ProtocolDetailViewController.m
+++ MedicalProtocols/ProtocolDetailViewController.m
@@ -102,8 +102,7 @@
 {
 
     UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"smallComponentCell" forIndexPath:indexPath];
-    ProtocolStep* step = [self.protocol stepAtIndex:indexPath.row];
-    Component *component = [step componentAtIndex:0];
+    Component *component = [self.step componentAtIndex:indexPath.row];
     ComponentView *componentView = [[ComponentView alloc]initWithFrame:cell.frame Object:component];
     componentView.center = CGPointMake(cell.frame.size.width/2, cell.frame.size.height/2);
     [cell addSubview:componentView];
