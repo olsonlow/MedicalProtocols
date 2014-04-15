bdf14326096260ffb8b4f8029f225f6022e38533
Lowell D. Olson <lowell@wrls-106-60.sonoma.edu>
2014-04-14 17:54:20 -0700

    Link works

--- MedicalProtocols/LinkView.m
+++ MedicalProtocols/LinkView.m
@@ -26,41 +26,25 @@
     NSLog(@"LINK OBJECT: %@", link.url);
     self.link = link;
     self.center = CGPointMake(frame.size.width/2, frame.size.height/2);
-    
     UIButton *linkButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    //linkButton.center = CGPointMake(frame.size.height/2, frame.size.width/4);
-    [linkButton setCenter:self.center];
-    linkButton.backgroundColor = [UIColor yellowColor];
-    [linkButton setFrame:CGRectMake(frame.origin.x, frame.origin.y, 100, 100)];
-    [linkButton setTitle:self.link.url forState:UIControlStateNormal];
-    //[linkButton setFont:[UIFont boldSystemFontOfSize:20]];
-   // [linkButton addTarget:self action:@selector(buttonPressed) forControlEvents:UIControlEventTouchUpInside];
-    UILabel *l = [[UILabel alloc]init];
-    [l setCenter:self.center];
-    [l setFrame:CGRectMake(frame.origin.x, frame.origin.y, 100, 100)];
-    l.text = @"HELLO";
-    [self addSubview:l];
+    linkButton.frame = CGRectMake(frame.origin.x, frame.origin.y, 100, 100);
+    linkButton.center = CGPointMake(linkButton.frame.size.height, linkButton.frame.size.width);
+    linkButton.backgroundColor = [UIColor darkGrayColor];
+    [linkButton setTitle:self.link.label forState:UIControlStateNormal];
+    [linkButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:linkButton];
     [self bringSubviewToFront:self];
     
-    
-    
-    
-//    self.backgroundColor = [UIColor colorWithRed:194.0 green:194.0 blue:194.0 alpha:1.0];
-//    self.backgroundColor = [UIColor blueColor];
-//    UILabel *label = [[UILabel alloc]init];
-//    label.text = self.link.label;
-//    UILabel *linkLabel = [[UILabel alloc]init];
-//    linkLabel.text = self.link.url;
-//    CGPoint labelCenter = CGPointMake(self.center.x/2, self.center.y/2);
-//    CGPoint linkCenter = CGPointMake(self.center.x/4, self.center.y/4);
-//    [label setCenter:labelCenter];
-//    [linkLabel setCenter:linkCenter];
-//    [self addSubview:label];
-//    [self addSubview:linkLabel];
     return self;
 }
 
+-(void)buttonPressed:(id)sender{
+    NSURL *url = [NSURL URLWithString:self.link.url];
+    
+    if (![[UIApplication sharedApplication] openURL:url])
+        
+        NSLog(@"%@%@",@"Failed to open url:",[url description]);
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
